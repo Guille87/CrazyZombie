13b0c9ae1928254ff928644c01cbb1f7492adbf0
Guille87 <guillermo_amado@hotmail.es>
2025-01-28 20:46:11 +0100

    Add player jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+
+    Rigidbody rb;
+    CapsuleCollider col;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
     }
     
     void Update()
@@ -17,11 +24,22 @@ public class PlayerMovement : MonoBehaviour
         moveInput.y = Input.GetAxis("Vertical") * speed;
         moveInput *= Time.deltaTime;
         transform.Translate(moveInput.x, 0, moveInput.y);
+
+        // Jump
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
         
         // Unlock the cursor
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
         }
+    }
+    
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 0.1f);
     }
 }
