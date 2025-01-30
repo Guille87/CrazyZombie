17d4a1e7f37d1d50920a279ac1d54891ba9ab3d3
Guille87 <guillermo_amado@hotmail.es>
2025-01-30 18:57:07 +0100

    Added impact effect particle system

--- Assets/Scripts/BulletHit.cs
+++ Assets/Scripts/BulletHit.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class BulletHit : MonoBehaviour
 {
+    [SerializeField] GameObject particleEffect;
+
     void OnCollisionEnter(Collision collision)
     {
+        Instantiate(particleEffect, transform.position, Quaternion.identity);
+
         gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/DestroyEffect.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DestroyEffect : MonoBehaviour
+{
+    const float TIME = 1;
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= TIME)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
